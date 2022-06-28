3e0c3dc6c4b86c0a71c4890153a393403fce6d3c
Eugene Rodion <90598304+uuujjin@users.noreply.github.com>
2022-06-28 18:30:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverseNum(n)
+{
+    if(n)
+    {
+        return reverseNum(Math.floor(n / 10),
+        (result*10)+(n % 10));
+    };
+    return result; 
+};
+
